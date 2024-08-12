b881a5709984ac83e6e899464ce7eb75a49eb1c0
skwwnlpro <skwwnlpro@gmail.com>
2024-08-12 09:42:25 +0900

    Feat: Delete test.py

--- Project/Django/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-header_user = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" }
-
-base_url = "https://search.naver.com/search.naver?ssc=tab.blog.all&sm=tab_jum&query="
