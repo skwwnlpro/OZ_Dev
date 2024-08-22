f86f3116ec27a4a44d50ac3419e7b5ac7d26f38d
skwwnlpro <skwwnlpro@gmail.com>
2024-08-22 11:51:59 +0900

    feat: chapter_01 Practice Upload

---
+++ Project/Django/Django-Assginment/chapter_01/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from fake_db import user_db
+
+
+# Create your views here.
+def user_list(request):
+    user = user_db
+    context = {"user_list": user}
+    return render(request, "user_list.html", context)
+
+
+def user_info(request, user_id):
+    user = user_db[user_id]
+    context = {"user": user}
+    return render(request, "user_info.html", context)
