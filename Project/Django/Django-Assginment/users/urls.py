a9de0598484a08b64038b2698ce5a55a0c1fc273
skwwnlpro <skwwnlpro@gmail.com>
2024-08-27 00:14:54 +0900

    Feat: Chapter_03 Practice Upload

--- Project/Django/Django-Assginment/users/urls.py
+++ Project/Django/Django-Assginment/users/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from users.views import sign_up, login
 
 urlpatterns = [
+    path("", include("django.contrib.auth.urls"), name="logout"),
     path("login/", login, name="login"),
-    path("logout/", include("django.contrib.auth.urls"), name="logout"),
     path("signup/", sign_up, name="signup"),
 ]
